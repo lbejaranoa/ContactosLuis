dde7d16f42966cc9bacaf834e62a40dff87b8a4a
ios3729 <ios3729@caelum57-03.local>
2013-10-30 19:16:07 -0200

    Initial Commit

---
+++ ContactosLuis/OMCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OMCAppDelegate.h
+//  ContactosLuis
+//
+//  Created by ios3729 on 30/10/13.
+//  Copyright (c) 2013 infinitodhd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OMCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ContactosLuis/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ContactosLuis
+//
+//  Created by ios3729 on 30/10/13.
+//  Copyright (c) 2013 infinitodhd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OMCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OMCAppDelegate class]));
+    }
+}
